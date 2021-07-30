d5d3bb3847d990466be6aa35bd1cb4757f0d6d50
Sandhya <Sandhya@192.168.0.21>
2021-07-30 01:12:01 -0700

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String shopping = "Ready";
+		
+		if(shopping == "Ready") 
+		   System.out.println("if ready then go for shopping");
+		else {
+			System.out.println("if not ready then no shopping");
+
+	}
+
+  }
+}
